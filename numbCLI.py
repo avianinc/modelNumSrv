8fe3b0c830dd5fdd4f8eeb9848b6c80dc914c0fb
John K. DeHart <jdehart@avian.com>
2023-08-09 21:08:55 -0400

    adding some clean up and a exe for the cli

---
+++ numbCLI.py
@@ -0,0 +1,110 @@
+import argparse
+import requests
+import cmd
+import configparser
+
+# Read from the config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+HOST = 'localhost'
+PORT = config.get('DEFAULT', 'PORT', fallback='5001')
+BASE_URL = f"http://{HOST}:{PORT}"
+
+class NumberingCLI(cmd.Cmd):
+    use_rawinput = True  # Add this line
+    intro = "Welcome to the model numbering CLI. Type help or ? to list commands.\n"
+    prompt = "(numbering) "
+
+    def do_add_model_type(self, model_type):
+        """Add a new model type."""
+        response = requests.post(f"{BASE_URL}/add_model_type/{model_type}")
+        print(response.json().get("status", "An error occurred."))
+
+    def do_list_model_types(self, _):
+        """List available model types."""
+        response = requests.get(f"{BASE_URL}/list_model_types")
+        model_types = response.json().get("model_types", [])
+        for model in model_types:
+            print(model)
+
+    def do_pull(self, model_type):
+        """Retrieve and reserve the next available number for a given model type."""
+        response = requests.get(f"{BASE_URL}/pull/{model_type}")
+        try:
+            number = response.json()["number"]
+            formatted_number = f"{model_type}-{number:04}"
+            print(formatted_number)
+        except:
+            print("Error:", response.json().get("error", "An error occurred."))
+
+    def do_confirm(self, arg):
+        """Confirm the use of a specific number for a given model type."""
+        try:
+            parser = argparse.ArgumentParser(prog="confirm", description="Confirm the use of a specific number for a given model type.")
+            parser.add_argument("model_number", help="Model number in the format MODEL-NUMBER", type=str)
+            args = parser.parse_args(arg.split())
+
+            model_type, number = args.model_number.split('-')
+            response = requests.post(f"{BASE_URL}/confirm/{model_type}/{number}")
+            if response.status_code == 200:
+                status = response.json()["status"]
+                print(f"{status}: {model_type}-{number}")
+            else:
+                print(response.json().get("error", "An error occurred."))
+        except (ValueError, argparse.ArgumentError):
+            print("Invalid input format. Please use the format MODEL-NUMBER, e.g., SYS-0001")
+
+    def do_release(self, arg):
+        """Release a specific number for a given model type."""
+        try:
+            parser = argparse.ArgumentParser(prog="release", description="Release a specific number for a given model type.")
+            parser.add_argument("model_number", help="Model number in the format MODEL-NUMBER", type=str)
+            args = parser.parse_args(arg.split())
+
+            model_type, number = args.model_number.split('-')
+            response = requests.post(f"{BASE_URL}/release/{model_type}/{number}")
+            if response.status_code == 200:
+                try:
+                    print(response.json()["status"])
+                except requests.exceptions.JSONDecodeError:
+                    print("Received non-JSON response:", response.text)
+            else:
+                try:
+                    print(response.json()["error"])
+                except requests.exceptions.JSONDecodeError:
+                    print("Error:", response.text)
+        except (ValueError, argparse.ArgumentError):
+            print("Invalid input format. Please use the format MODEL-NUMBER, e.g., SYS-0001")
+
+
+    def do_search(self, arg):
+        """Search for a specific number for a given model type."""
+        try:
+            parser = argparse.ArgumentParser(prog="search", description="Search for a specific number for a given model type.")
+            parser.add_argument("model_number", help="Model number in the format MODEL-NUMBER", type=str)
+            args = parser.parse_args(arg.split())
+
+            model_type, number = args.model_number.split('-')
+            response = requests.get(f"{BASE_URL}/search/{model_type}/{number}")
+            if response.status_code == 200:
+                status = response.json()["status"]
+                print(f"Model {model_type}, Number {number}: {status}")
+            else:
+                print(response.json()["error"])
+        except (ValueError, argparse.ArgumentError):
+            print("Invalid input format. Please use the format MODEL-NUMBER, e.g., SYS-0001")
+
+
+    def do_exit(self, _):
+        """Exit the CLI."""
+        return True
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Model Numbering CLI")
+    parser.add_argument("--interactive", action="store_true", help="Enter interactive mode")
+    args = parser.parse_args()
+    if args.interactive:
+        NumberingCLI().cmdloop()
+    else:
+        print("Run with --interactive to enter interactive mode or use the API endpoints directly.")

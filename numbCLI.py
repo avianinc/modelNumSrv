4875fb0107367841f8620d08040076d30cdd15e2
John K. DeHart <jdehart@avian.com>
2023-08-10 09:04:46 -0400

    shit

--- numbCLI.py
+++ numbCLI.py
@@ -2,19 +2,40 @@ import argparse
 import requests
 import cmd
 import configparser
+import os
 
-# Read from the config file
+# Initial read from the config file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 HOST = 'localhost'
 PORT = config.get('DEFAULT', 'PORT', fallback='5001')
-BASE_URL = f"http://{HOST}:{PORT}"
+BASE_URL = f"http://{HOST}"
 
 class NumberingCLI(cmd.Cmd):
     use_rawinput = True  # Add this line
     intro = "Welcome to the model numbering CLI. Type help or ? to list commands.\n"
     prompt = "(numbering) "
+
+    def do_set_base_url(self, new_url):
+        """Set a new base URL for the CLI."""
+        global BASE_URL, PORT
+        
+        # Check if user explicitly specifies a port.
+        if ':' in new_url.split("//")[-1]:
+            BASE_URL, PORT = new_url.rsplit(':', 1)
+        else:
+            BASE_URL = new_url
+            PORT = config.get('DEFAULT', 'PORT', fallback='5001')
+        
+        # Update the config.ini
+        config['DEFAULT']['base_url'] = f"{BASE_URL}:{PORT}"
+        config['DEFAULT']['port'] = PORT
+
+        with open('config.ini', 'w') as configfile:  # Save the updated base URL and port to the config file
+            config.write(configfile)
+
+        print(f"Base URL set to: {BASE_URL}:{PORT}")
 
     def do_add_model_type(self, model_type):
         """Add a new model type."""
@@ -30,8 +51,10 @@ class NumberingCLI(cmd.Cmd):
 
     def do_pull(self, model_type):
         """Retrieve and reserve the next available number for a given model type."""
-        response = requests.get(f"{BASE_URL}/pull/{model_type}")
+        url = f"{BASE_URL}:{PORT}/pull/{model_type}"
+        # print(f"Trying to access: {url}")  # Diagnostic print
         try:
+            response = requests.get(url)
             number = response.json()["number"]
             formatted_number = f"{model_type}-{number:04}"
             print(formatted_number)
@@ -113,6 +136,23 @@ class NumberingCLI(cmd.Cmd):
         except (ValueError, argparse.ArgumentError):
             print("Invalid input format. Please use the format MODEL-NUMBER, e.g., SYS-0001")
 
+    def postcmd(self, stop, line):
+        """This method is called after any command."""
+        if hasattr(self, '_connection_error'):
+            print("Error: Could not establish a connection to the server. Please check the BASE URL and ensure the server is running.")
+            delattr(self, '_connection_error')  # remove the flag to reset the state
+        return stop
+
+    def onecmd(self, line):
+        """Execute one command."""
+        try:
+            return super(NumberingCLI, self).onecmd(line)
+        except requests.exceptions.ConnectionError:
+            self._connection_error = True  # set a flag indicating there was a connection error
+        except KeyboardInterrupt:
+            print("\nExiting CLI.")
+            return True
+
     def do_exit(self, _):
         """Exit the CLI."""
         return True
@@ -122,6 +162,12 @@ if __name__ == "__main__":
     parser.add_argument("--interactive", action="store_true", help="Enter interactive mode")
     args = parser.parse_args()
     if args.interactive:
-        NumberingCLI().cmdloop()
+        try:
+            NumberingCLI().cmdloop()
+        except requests.exceptions.ConnectionError:
+            print("Error: Could not establish a connection to the server. Please check the BASE URL and ensure the server is running.")
+        except KeyboardInterrupt:
+            # Handle Ctrl+C gracefully
+            print("\nExiting CLI.")
     else:
         print("Run with --interactive to enter interactive mode or use the API endpoints directly.")
